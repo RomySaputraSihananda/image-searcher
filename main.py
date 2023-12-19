941db66c24742c24381b998d59ce6c7a19c06738
romys <romysaputrasihanandaa@gmail.com>
2023-12-19 21:46:25 +0700

    update

--- main.py
+++ main.py
@@ -1,27 +1,7 @@
-import uuid
-import os
+from fastapi import FastAPI
 
-from typing import Union
-from fastapi import FastAPI, File, UploadFile, templating, Request
-
-from libs import Google
+from libs.controllers import Google
 
 app = FastAPI()
 
-template = templating.Jinja2Templates(directory='templates')
-
-@app.get("/")
-def read_root(request: Request):
-    return template.TemplateResponse('index.html', { 'request': request })
-
-@app.post('/upload')
-async def upload(gambar: UploadFile = File(...)):
-    name = str(uuid.uuid4()) + '.' + gambar.filename.split('.')[-1]
-    with open(f'img/{name}', 'wb') as f:
-        f.write(gambar.file.read())
-
-    data = Google().start(f'{os.getcwd()}/img/{name}')
-
-    os.remove(f'{os.getcwd()}/img/{name}')
-
-    return data
+app.include_router(Google.router, prefix="/google", tags=["Google Search image"])
